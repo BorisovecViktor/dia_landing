df4a283de72fc9b2afd4505bbb7ba9bdb413a9d1
BorisovecViktor <viktorborisovets@gmail.com>
2020-06-28 16:48:55 +0300

    production

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,39 +1,54 @@
 'use strict';
 
-const $btn = document.getElementById('show');
-const $nav = document.getElementById('nav');
-const $link = document.querySelector('.nav__list');
-const $scrollButton = document.querySelector('.scroll-button');
+const btn = document.getElementById('show');
+const nav = document.getElementById('nav');
+const sticky = document.querySelector('.header');
+const link = document.querySelector('.nav__list');
+const scrollButton = document.querySelector('.scroll-button');
+const formFeedback = document.querySelector('.message__form');
 
-$btn.addEventListener('click', function() {
-  $nav.classList.toggle('active');
-  $btn.classList.toggle('open');
+btn.addEventListener('click', function() {
+  nav.classList.toggle('active');
+  btn.classList.toggle('open');
 });
 
-$link.addEventListener('click', function() {
-  $nav.classList.remove('active');
-  $btn.classList.remove('open');
+link.addEventListener('click', function() {
+  nav.classList.remove('active');
+  btn.classList.remove('open');
 });
 
-// $(document).ready(function(){
-//   $(window).scroll(function () {
-//     if ($(this).scrollTop() > 5000) {
-//       $('#header').css('visibility', 'hidden');
-//     } else {
-//       $('#header').css('visibility', 'visible');
-//     }
-//   });
-// });
+formFeedback.addEventListener('submit', (e) => {
+  e.preventDefault();
+  formFeedback.reset();
+  swal({
+    title: "Good job!",
+    text: "Your message has been sent!",
+    icon: "success",
+    button: "Close",
+  });
+});
 
 window.onscroll = function() {
-  if (window.pageYOffset > 700) {
-    $scrollButton.classList.add('show');
+  if (window.pageYOffset > 5000) {
+    scrollButton.classList.add('show');
+    sticky.classList.add('hidden');
   } else {
-    $scrollButton.classList.remove('show');
+    scrollButton.classList.remove('show');
+    sticky.classList.remove('hidden');
+  }
+
+  if (window.matchMedia('(max-width: 1023px)').matches) {
+    sticky.classList.remove('hidden');
+
+    if (window.pageYOffset > 2000) {
+      scrollButton.classList.add('show');
+    } else {
+      scrollButton.classList.remove('show');
+    }
   }
 };
 
-$scrollButton.addEventListener('click', function() {
+scrollButton.addEventListener('click', function() {
   document.documentElement.scrollIntoView({
     behavior: 'smooth',
     block: 'start',
